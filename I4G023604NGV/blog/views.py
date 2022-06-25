4ae8d93392dc76023036152b6e8a28b7fb352354
Obianuju <obianujuofodu@gmail.com>
2022-06-25 23:16:51 +0100

    final update

--- I4G023604NGV/blog/views.py
+++ I4G023604NGV/blog/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Post
+
+
+class PostListView(CreateView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = “__all__”
+    success_url  = reverse_lazy(“blog:all”)
+
+
+class PostDetailView(CreateView):
+    model = Post
+
+class PostUpdateView(CreateView):
+    model = Post
+    fields = “__all__”
+    success_url  = reverse_lazy(“blog:all”)
+
+
+class PostDeleteView(CreateView):
+    model = Post
+    fields = “__all__”
+    success_url  = reverse_lazy(“blog:all”)
+
+
 
 # Create your views here.
